81580fb8daf9510b6656bb4ae4f4958d061af4fb
teafly <orgteafly@gmail.com>
2012-08-22 11:17:36 +0800

    update

--- lib/tdev/tdev.rb
+++ lib/tdev/tdev.rb
@@ -3,26 +3,14 @@
 require "yaml"
 require "readline"
 require "fileutils"
+require 'optparse'
 
 env_dir = ENV["HOME"] + "/.tdev"
 $env = YAML.load(File.open(env_dir))
 
 module Tdev
 
-  class App
-
-
-    def initialize(dir)
-
-      @dir = File.expand_path(dir)
-      @name = /.*\/app\.(.*)/.match(@dir)[1]
-      @dev_appdir = "#{$env["dev_workdir"]}/intl-#{@name}"
-
-      prop_map = $env["prop_map"]
-      key = @name.split("_")[0]
-      config_name = prop_map[key].nil? ? @name + ".prop" : prop_map[key] + ".prop"
-      @conf_dir = File.expand_path($env["config_dir"] + "/" + config_name)
-    end
+  class BaseApp
 
     def cp_conf
 
@@ -79,6 +67,51 @@ module Tdev
       system "rsync -av --delete --progress #{@dir}/deploy #{$env["dev_addr"]}:#{@dev_appdir} --exclude 'logs' "
     end
 
+
+  end
+
+  class Standalone < BaseApp
+
+    def initialize(dir)
+
+      @dir = File.expand_path(dir)
+      @name = /.*\/(standalone\..*)/.match(@dir)[1]
+      @dev_appdir = "#{$env["dev_workdir"]}/#{@name}"
+
+      prop_map = $env["prop_map"]
+      key = @name.split("_")[0]
+      config_name = prop_map[key].nil? ? @name + ".prop" : prop_map[key] + ".prop"
+      @conf_dir = File.expand_path($env["config_dir"] + "/" + config_name)
+    end
+
+    def deploy
+
+      build
+      push
+    end
+
+    def deploy_cfg
+
+      config
+      push
+    end
+  end
+
+  class App < BaseApp
+
+
+    def initialize(dir)
+
+      @dir = File.expand_path(dir)
+      @name = /.*\/app\.(.*)/.match(@dir)[1]
+      @dev_appdir = "#{$env["dev_workdir"]}/intl-#{@name}"
+
+      prop_map = $env["prop_map"]
+      key = @name.split("_")[0]
+      config_name = prop_map[key].nil? ? @name + ".prop" : prop_map[key] + ".prop"
+      @conf_dir = File.expand_path($env["config_dir"] + "/" + config_name)
+    end
+
     def run
 
       bin_dir = "#{@dev_appdir}/deploy/bin"
@@ -101,6 +134,7 @@ module Tdev
     end
   end
 
+
   class Module
 
     def initialize(dir)
@@ -116,11 +150,11 @@ module Tdev
 
   class Project
 
-    attr_accessor :mods, :apps
+    attr_accessor :mods, :apps, :stndalones
     def initialize(dir)
 
       @dir = File.expand_path(dir)
-      @mod_list, @app_list = [], []
+      @mod_list, @app_list, @standalone_list = [], [], []
 
       Dir.entries(@dir).each do |file|
 
@@ -136,6 +170,9 @@ module Tdev
         elsif "app".eql? type
 
           @app_list.push file
+        elsif "standalone".eql? type
+
+          @standalone_list.push file
         end
       end
     end
@@ -164,6 +201,17 @@ module Tdev
         @apps = Readline.readline(">> ").chomp
       end
 
+      if @standalone_list.size > 0
+
+        puts "Standalone list: "
+        @standalone_list.each_with_index do |file, index|
+          puts "[#{index}]\t#{file}"
+        end
+
+        puts "Please input the standalone number you want to build and split by ' '."
+        @standalones = Readline.readline(">> ").chomp
+      end
+
     end
 
     def build
@@ -185,6 +233,15 @@ module Tdev
           Tdev::App.new(task_dir).build
         end
       end
+
+      unless @standalones.nil?  || @standlaones.empty?
+
+        @standalones.split("\s").each do |num|
+
+          task_dir = @dir + "/" + @standalone_list[num.to_i]
+          Tdev::Standalone.new(task_dir).build
+        end
+      end
     end
 
     def deploy
@@ -200,7 +257,175 @@ module Tdev
           Tdev::App.new(task_dir).deploy
         end
       end
+
+      unless @standalones.nil?  || @standaones.empty?
+
+        @standalones.split("\s").each do |num|
+
+          task_dir = @dir + "/" + @standalone_list[num.to_i]
+          Tdev::Standalone.new(task_dir).deploy
+        end
+      end
     end
 
   end
+end
+
+base_dir = File.expand_path($env['workdir'])
+
+dir = Dir.pwd
+
+group = /#{base_dir}\/?([^\/]*)\/?([^\/]*)/.match(dir)
+
+if group.nil?
+
+	$mod = 'other'
+elsif !group[2].nil? && !group[2].empty?
+
+    grp = /(\w+)\..+/.match(group[2])
+
+    unless grp.nil? || grp.size  < 2
+
+      type = grp[1]
+
+      if "modules".eql? type
+
+      	$mod = 'module'
+      elsif "app".eql? type
+
+      	$mod = 'app'
+      elsif "standalone".eql? type
+
+        $mod = 'standalone'
+      end
+    end
+elsif !group[1].nil? && !group[1].empty?
+
+  $mod = 'project'
+else
+
+	$mod = 'other'
+end
+
+puts "Current mod is \"#{$mod}\""
+
+module_op = OptionParser.new do |opts|
+
+  opts.banner = 'Here is help messages of the tdev tool.'
+
+  opts.on do
+    Tdev::Module.new(dir).build
+  end
+
+  opts.on('build', 'build module') do
+    Tdev::Module.new(dir).build
+  end
+
+end
+
+app_op = OptionParser.new do |opts|
+
+  opts.banner = 'Here is help messages of the tdev tool.'
+
+  opts.on do
+    Tdev::App.new(dir).deploy
+  end
+
+  opts.on('cp-conf', 'copy the config to app') do
+    Tdev::App.new(dir).cp_conf
+  end
+
+  opts.on('config', 'config app') do
+    Tdev::App.new(dir).config
+  end
+
+  opts.on('build', 'build app') do
+    Tdev::App.new(dir).build
+  end
+
+  opts.on('push', 'push app to dev') do
+    Tdev::App.new(dir).push
+  end
+
+  opts.on('run', 'run app') do
+    Tdev::App.new(dir).run
+  end
+
+  opts.on('deploy', 'deploy app') do
+    Tdev::App.new(dir).deploy
+  end
+
+  opts.on('deploy-cfg', 'deploy app only config') do
+    Tdev::App.new(dir).deploy_cfg
+  end
+
+end
+
+standalone_op = OptionParser.new do |opts|
+
+  opts.banner = 'Here is help messages of the tdev tool.'
+
+  opts.on do
+    Tdev::Standalone.new(dir).deploy
+  end
+
+  opts.on('cp-conf', 'copy the config to standalone') do
+    Tdev::Standalone.new(dir).cp_conf
+  end
+
+  opts.on('config', 'config standalone') do
+    Tdev::Standaone.new(dir).config
+  end
+
+  opts.on('build', 'build standalone') do
+    Tdev::Standalone.new(dir).build
+  end
+
+  opts.on('push', 'push app to standalone') do
+    Tdev::Standalone.new(dir).push
+  end
+
+  opts.on('deploy', 'deploy standalone') do
+    Tdev::Standalone.new(dir).deploy
+  end
+
+  opts.on('deploy-cfg', 'deploy standalone only config') do
+    Tdev::Standalone.new(dir).deploy_cfg
+  end
+
+end
+
+proj_op = OptionParser.new do |opts|
+
+  opts.banner = 'Here is help messages of the tdev tool.'
+
+  opts.on do
+    Tdev::Project.new(dir).deploy
+  end
+
+  opts.on('init', 'init project') do
+    Tdev::Project.new(dir).init
+  end
+
+  opts.on('build', 'build project') do
+    Tdev::Project.new(dir).build
+  end
+
+  opts.on('deploy', 'deploy project') do
+    Tdev::Project.new(dir).deploy
+  end
+
+end
+
+case $mod 
+when 'project'
+  proj_op.parse!
+when 'app'
+  app_op.parse!
+when 'standalone'
+  standalone_op.parse!
+when 'module'
+  module_op.parse!
+when 'other'
+  puts 'hello, teafly!'
 end
